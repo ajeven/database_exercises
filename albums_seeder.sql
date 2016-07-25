d066dccde6c7ff0fd1cccb7e46de1dd0e6b2b357
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-25 16:04:52 -0500

    "creat sql table data with insert script"

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+INSERT INTO albums  VALUES
+					(1, 'Micheal Jackson', 'Thriller', '1982-11-30', 65, 'pop rock r&b'),
+					(2, 'Pink Floyd', 'Dark Side of The Moon', '1980-03-01', 45, 'prog rock'),
+					(3, 'The Beatles', 'Sgt Peppers', '1967-06-01', 32, 'rock'),
+					(4, 'Nirvana', 'Nevermind', '1991-09-24', 30, 'grunge rock'); 
