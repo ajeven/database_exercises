afba1aa68150e0338a8af113fe4018949cddba8f
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-27 12:11:50 -0500

    "more where exercises"

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,8 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Maya', 'Vidya');
-SELECT * FROM employees WHERE last_name LIKE "E%";
-SELECT * FROM employees WHERE hire_date LIKE "199%";
+SELECT * FROM employees WHERE gender = "M" AND (first_name = 'Irena' OR first_name = 'Maya' OR first_name = 'Vidya');
+SELECT * FROM employees WHERE last_name LIKE "E%" OR last_name LIKE "%e";
+SELECT * FROM employees WHERE last_name LIKE "E%e";
+SELECT * FROM employees WHERE hire_date LIKE "199%" AND birth_date LIKE "%%%%-12-25";
 SELECT * FROM employees WHERE birth_date  LIKE "%%%%-12-25";
-SELECT * FROM employees WHERE last_name LIKE "%q%";
+SELECT * FROM employees WHERE last_name LIKE "%q%" AND (last_name NOT LIKE "%qu%");
